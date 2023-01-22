ebda26efd2c18cd2cb7919c96dc38b904f9c305f
Max Wiesenfeld <maw5859@utexas.edu>
2023-01-22 12:05:08 -0600

    Reorganized files for future packages

--- stack/postgres_proto/paho_testing.py
+++ stack/postgres_proto/paho_testing.py
@@ -1,7 +1,18 @@
 from paho.mqtt import client as mqtt_client
+from numpy.random import default_rng
 import time
 import math
 import json
+
+# class DataTester:
+    
+
+
+def get_random_data(dtype, num, *minmax):
+    rng = default_rng()
+    if dtype is bool:
+        return rng.integers(0, 2, size=num).astype(bool)
+    return rng.integers(*minmax, size=num)
 
 
 def main():
@@ -12,9 +23,9 @@ def main():
         time.sleep(1)
         # obj = {'val': math.sin(time.time() * .5)}
         obj = {'creation_time': int(time.time() * 1000000), 'val': math.sin(time.time() * .5)}
-        client.publish('paho_test', json.dumps(obj, indent=4))
+        client.publish('drive_day', json.dumps(obj, indent=4))
         print(f'Outputting sine val ({obj["val"]}) now...')
         count += 1
 
 if __name__ == '__main__':
-    main()
+    print(get_random_data(bool, 10, 15))
