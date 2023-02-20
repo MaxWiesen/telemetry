ffc8424443b42aafbf350c8bf7766871c9cdf9c1
rohansnayak <115941670+rohansnayak@users.noreply.github.com>
2023-02-19 18:48:26 -0600

    Added Website Files

---
+++ server copy.py
@@ -0,0 +1,37 @@
+import sqlite3
+from flask import Flask, render_template
+from werkzeug.exceptions import abort
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html')
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+# https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
