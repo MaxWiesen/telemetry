d75a9c0e2706a25224651553fb1c2b1e502f9005
Max Wiesenfeld <maw5859@utexas.edu>
2023-03-19 00:16:06 -0400

    Added drive day ID propogation and checking logic

--- analysis/database/server/one_time_injection.py
+++ analysis/database/server/one_time_injection.py
@@ -1,14 +1,27 @@
 import psycopg
+from datetime import datetime
+from analysis.database.sql_utils.db_handler import DBHandler
 
 
 class Injector:
-    def __init__(self, **kwargs):
-        pass
+    # def __init__(self, **kwargs):
+
+
 
     @staticmethod
     def create_drive_day(**kwargs):
-        print(kwargs)
-
+        cnx = DBHandler().connect()
+        try:
+            cur = cnx.cursor()
+            cur.execute(f'''INSERT INTO drive_day (time, conditions, power_limit)
+                            VALUES ({datetime.now()}, {kwargs.get("conditions")}, {kwargs.get("power_limit")})
+                            RETURNING day_id''')
+            DBHandler.kill(cnx)
+            return cur.fetchone()[0]
+        except Exception as e:
+            raise e
+        finally:
+            DBHandler.kill(cnx)
 
 # if __name__ == '__main__':
 #     main()
