da7de0b32382f7f360c6153f689ae8d8ab1cb827
Max Wiesenfeld <maw5859@utexas.edu>
2023-04-16 16:16:14 -0500

    Adapted to DB v2.0

--- analysis/database/sql_utils/db_handler.py
+++ analysis/database/sql_utils/db_handler.py
@@ -2,91 +2,86 @@ import psycopg
 import logging
 import time
 import numpy as np
+import datetime
 
 
 class TableSpecs:
     table_column_specs = {  # Last Updated for DB schema v1.2
         'drive_day': {
             'day_id': {'type': int, 'range': (0, 1_000)},
-            'time': {'type': float, 'range': (time.time(), time.time())},
             'conditions': {'type': str},
-            'power_limit': {'type': np.float32, 'range': (0, 80_000)}
+            'power_limit': {'type': int, 'range': (0, 80_000)},
+            'date': {'type': datetime.datetime, 'range': (datetime.date.today().isoformat(), datetime.date.today().isoformat())},
+
         },
         'event': {
             'event_id': {'type': int, 'range': (0, 1_000)},
+            'drive_day': {'type': int, 'range': (0, 1_000)},
             'creation_time': {'type': float, 'range': (time.time(), time.time())},
             'start_time': {'type': float, 'range': (time.time(), time.time())},
             'end_time': {'type': float, 'range': (time.time(), time.time())},
-            'drive_day': {'type': int, 'range': (0, 1_000)},
-            'driver': {'type': int, 'range': (0, 10)},
-            'location': {'type': int, 'range': (0, 10)},
-            'event_index': {'type': int, 'range': (0, 15)},
+            'car_id': {'type': int, 'range': (0, 2)},
+            'driver_id': {'type': int, 'range': (0, 4)},
+            'location_id': {'type': int, 'range': (0, 8)},
             'event_type': {'type': int, 'range': (0, 5)},
-            # 'car_weight': {'type': np.float32, 'range': (100.0, 3_000)},
+            'event_index': {'type': int, 'range': (0, 15)},
+            'car_weight': {'type': np.float32, 'range': (100.0, 3_000)},
+            'tow_angle': {'type': np.float32, 'range': (0, 22.5)},
+            'camber': {'type': np.float32, 'range': (0, 0)},
+            'ride_height': {'type': np.float32, 'range': (0, 0)},
+            'ackerman_adjustment': {'type': np.float32, 'range': (0, 0)},
+            'power_output': {'type': int, 'range': ()},
+            'torque_output':  {'type': int, 'range': ()},
+            'shock_dampening': {'type': int, 'range': ()},
+            'frw_pressure': {'type': float, 'range': (0, 100)},
+            'flw_pressure': {'type': float, 'range': (0, 100)},
+            'brw_pressure': {'type': float, 'range': (0, 100)},
+            'blw_pressure': {'type': float, 'range': (0, 100)},
             'front_wing_on': {'type': bool},
-            'regen_on': {'type': bool},
-            # 'tow_angle': {'type': np.float32, 'range': (0, 22.5)},
-            # 'camber': {'type': np.float32, 'range': (0, 0)},
-            # 'ride_height': {'type': np.float32, 'range': (0, 0)},
-            # 'ackerman_adjustment': {'type': np.float32, 'range': (0, 0)},
-            'tire_pressure': {'type': np.float32, 'range': (10, 50)},
-            # 'blade_arb_stiffness': {'type': np.float32, 'range': (0, 0)}
-            # 'power_output': {'type': int, 'range': ()},
-            # 'torque_output':  {'type': int, 'range': ()},
-            # 'shock_dampening': {'type': int, 'range': ()},
             'rear_wing_on': {'type': bool},
+            'regen_on': {'type': bool},
             'undertray_on': {'type': bool}
         },
         'dynamics': {
-            'event_id': {'type': int, 'range': (0, 1_000)},
             'time': {'type': float, 'range': (time.time(), time.time())},
-            'body_acc_x': {'type': np.float64, 'range': (0, 4)},
-            'body_acc_y': {'type': np.float64, 'range': (0, 4)},
-            'body_acc_z': {'type': np.float64, 'range': (0, 4)},
-            'body_ang_x': {'type': np.float64, 'range': (-180, 180)},
-            'body_ang_y': {'type': np.float64, 'range': (-180, 180)},
-            'body_ang_z': {'type': np.float64, 'range': (-180, 180)},
-            'fr_acc_x': {'type': np.float64, 'range': (0, 4)},
-            'fr_acc_y': {'type': np.float64, 'range': (0, 4)},
-            'fr_acc_z': {'type': np.float64, 'range': (0, 4)},
-            'fl_acc_x': {'type': np.float64, 'range': (0, 4)},
-            'fl_acc_y': {'type': np.float64, 'range': (0, 4)},
-            'fl_acc_z': {'type': np.float64, 'range': (0, 4)},
-            'br_acc_x': {'type': np.float64, 'range': (0, 4)},
-            'br_acc_y': {'type': np.float64, 'range': (0, 4)},
-            'br_acc_z': {'type': np.float64, 'range': (0, 4)},
-            'bl_acc_x': {'type': np.float64, 'range': (0, 4)},
-            'bl_acc_y': {'type': np.float64, 'range': (0, 4)},
-            'bl_acc_z': {'type': np.float64, 'range': (0, 4)},
-            # 'torque_command': {'type': np.float64, 'range': ()},
+            'frw_acc': {'type': np.empty(3, float), 'range': (0, 5)},
+            'flw_acc': {'type': np.empty(3, float), 'range': (0, 5)},
+            'brw_acc': {'type': np.empty(3, float), 'range': (0, 5)},
+            'blw_acc': {'type': np.empty(3, float), 'range': (0, 5)},
+            'body1_acc': {'type': np.empty(3, float), 'range': (0, 5)},
+            'body1_ang': {'type': np.empty(3, float), 'range': (0, 5)},
+            'body2_acc': {'type': np.empty(3, float), 'range': (0, 5)},
+            'body2_ang': {'type': np.empty(3, float), 'range': (0, 5)},
+            'body3_acc': {'type': np.empty(3, float), 'range': (0, 5)},
+            'body3_ang': {'type': np.empty(3, float), 'range': (0, 5)},
+            'accel_pedal_pos': {'type': float, 'range': (0, 5)},
+            'brake_pressure': {'type': float, 'range': (0, 5)},
             'motor_rpm': {'type': int, 'range': (0, 10_000)},
-            'tire_temp': {'type': np.float64, 'range': (0, 500)},
-            'brake_rotor_temp': {'type': np.float64, 'range': (0, 500)},
+            'torque_command': {'type': np.float64, 'range': (0, 100)},
             'gps': {'type': 'point'}
         },
         'power': {
-            # 'event_id': {'type': int, 'range': (0, 1_000)},
-            'event_id': {'type': int, 'range': (1, 2)},
-            'time': {'type': float, 'range': (time.time(), time.time())},
+            'time': {'type': int, 'range': (time.time(), time.time())},
+            'bms_cells_v': {'type': np.empty(3, np.float32), 'range': (0, 10)},
             'pack_voltage': {'type': np.float64, 'range': (0, 1_000)},
             'pack_current': {'type': np.float64, 'range': (0, 1_000)},
-            'dcdc_current': {'type': np.float64, 'range': (0, 1_000)},
+            'lv_cells_v': {'type': np.empty(3, np.float32), 'range': (0, 10)},
+            'lv_voltage': {'type': np.float64, 'range': (0, 1_000)},
+            'lv_current': {'type': np.float64, 'range': (0, 1_000)},
             'ambient_temp': {'type': np.float64, 'range': (0, 200)},
-            'bms_pack_temp': {'type': np.float64, 'range': (0, 300)},  # Array, jsonb, or scalar?
-            'bms_balancing_temp': {'type': np.float64, 'range': (0, 300)},  # Array, jsonb, or scalar?
-            'contactor_status': {'type': bool},
-            'bms_cells_v': {'type': np.empty((3, 3), np.float32), 'range': (0, 10)},
-            'fan_speed': {'type': np.int64, 'range': (0, 10_000)},
+            'bms_pack_temp': {'type': np.empty(3, np.float32), 'range': (0, 10)},  # Array, jsonb, or scalar?
+            'bms_balancing_temp': {'type': np.empty(3, np.float32), 'range': (0, 10)},  # Array, jsonb, or scalar?
+            'fan_speed': {'type': np.empty(3, np.float32), 'range': (0, 10)},
             'inline_cooling_temp': {'type': np.float64, 'range': (-50, 300)},
-            # 'cooling_flow': {'type': np.float64, 'range': ()}
+            'cooling_flow': {'type': np.float64, 'range': (0, 10)}
         },
         'electronics': {
-            'event_id': {'type': int, 'range': (0, 1_000)},
-            'time': {'type': float, 'range': (time.time(), time.time())},
+            'time': {'type': int, 'range': (time.time(), time.time())},
             'imd_on': {'type': bool},
             'hv_contactor_on': {'type': bool},
             'pre_c_contactor_on': {'type': bool},
-            # 'pcb_temps': {'type': },
+            'ls_contactor_on': {'type': bool},
+            'lv_battery_status': {'type': int, 'range': (0, 5)}
         }
     }
 
@@ -99,7 +94,9 @@ class DBHandler:
                 'electric': '2fast2quick',
                 'grafana': 'frontend',
                 'analysis': 'north_dakota'
-            }
+            },
+            'host': 'localhost',
+            'port': 5432
         }
     }
 
@@ -109,11 +106,14 @@ class DBHandler:
     def connect(self, target='PROD', user='analysis'):
         if target not in self.DB_CONFIG:
             raise ValueError(f'Target server {target} was not contained in DB_CONFIG.')
-        return psycopg.connect(dbname=self.DB_CONFIG[target]['dbname'], user=user,
-                               password=self.DB_CONFIG[target]['user'][user])
+        config = self.DB_CONFIG[target]
+        return psycopg.connect(dbname=config['dbname'], user=user, password=config['user'][user],
+                               host=config['host'], port=config['port'])
 
     @staticmethod
-    def kill(cnx):
+    def kill(cnx, commit=True):
+        if commit:
+            cnx.commit()
         if not cnx.closed:
             cnx.close()
         else:
@@ -121,5 +121,13 @@ class DBHandler:
 
 
 if __name__ == '__main__':
-    conn = DBHandler().connect()
-    print(f'Conn closed 1: {conn.closed}')
+    try:
+        handler = DBHandler()
+        cnx = handler.connect(user='electric')
+        with cnx.cursor() as cur:
+            cur.execute(f"INSERT INTO event (creation_time) VALUES ('{datetime.datetime.now()}') RETURNING event_id")
+            print(cur.fetchone())
+    except Exception as e:
+        raise e
+    finally:
+        DBHandler.kill(cnx)
---
+++ stack/db_init_v2.0.sql
@@ -0,0 +1,283 @@
+-- Database generated with pgModeler (PostgreSQL Database Modeler).
+-- pgModeler version: 1.0.1
+-- PostgreSQL version: 15.0
+-- Project Site: pgmodeler.io
+-- Model Author: ---
+-- -- object: electric | type: ROLE --
+-- -- DROP ROLE IF EXISTS electric;
+-- CREATE ROLE electric WITH 
+-- 	SUPERUSER
+-- 	CREATEDB
+-- 	CREATEROLE
+-- 	INHERIT
+-- 	LOGIN
+-- 	REPLICATION
+-- 	BYPASSRLS
+-- 	 PASSWORD '2fast2quick';
+-- -- ddl-end --
+-- 
+-- object: grafana | type: ROLE --
+-- DROP ROLE IF EXISTS grafana;
+CREATE ROLE grafana WITH 
+	LOGIN
+	 PASSWORD 'frontend'
+	CONNECTION LIMIT 5;
+-- ddl-end --
+
+-- object: analysis | type: ROLE --
+-- DROP ROLE IF EXISTS analysis;
+CREATE ROLE analysis WITH 
+	LOGIN
+	 PASSWORD 'north_dakota'
+	CONNECTION LIMIT 5;
+-- ddl-end --
+
+
+-- Database creation must be performed outside a multi lined SQL file. 
+-- These commands were put in this file only as a convenience.
+-- 
+-- object: telemetry | type: DATABASE --
+-- DROP DATABASE IF EXISTS telemetry;
+-- CREATE DATABASE telemetry
+-- 	OWNER = electric;
+-- ddl-end --
+
+
+-- object: public.drive_day | type: TABLE --
+-- DROP TABLE IF EXISTS public.drive_day CASCADE;
+CREATE TABLE public.drive_day (
+	day_id smallint NOT NULL,
+	date date NOT NULL,
+	power_limit integer,
+	conditions text,
+	CONSTRAINT drive_day_pk PRIMARY KEY (day_id)
+);
+-- ddl-end --
+ALTER TABLE public.drive_day OWNER TO electric;
+-- ddl-end --
+
+-- object: public.lut_driver | type: TABLE --
+-- DROP TABLE IF EXISTS public.lut_driver CASCADE;
+CREATE TABLE public.lut_driver (
+	driver_id smallint NOT NULL,
+	driver_name text NOT NULL,
+	driver_weight smallint,
+	CONSTRAINT lut_driver_pk PRIMARY KEY (driver_id)
+);
+-- ddl-end --
+ALTER TABLE public.lut_driver OWNER TO electric;
+-- ddl-end --
+
+INSERT INTO public.lut_driver (driver_id, driver_name, driver_weight) VALUES (E'0', E'Other', DEFAULT);
+-- ddl-end --
+INSERT INTO public.lut_driver (driver_id, driver_name, driver_weight) VALUES (E'1', E'Jaiden Patel', DEFAULT);
+-- ddl-end --
+INSERT INTO public.lut_driver (driver_id, driver_name, driver_weight) VALUES (E'2', E'Sohan Agnihotri', DEFAULT);
+-- ddl-end --
+INSERT INTO public.lut_driver (driver_id, driver_name, driver_weight) VALUES (E'3', E'Dylan Hammerback', DEFAULT);
+-- ddl-end --
+INSERT INTO public.lut_driver (driver_id, driver_name, driver_weight) VALUES (E'4', E'Andrew Zhang', DEFAULT);
+-- ddl-end --
+INSERT INTO public.lut_driver (driver_id, driver_name, driver_weight) VALUES (E'5', E'Ali Jensen', DEFAULT);
+-- ddl-end --
+
+-- object: public.lut_location | type: TABLE --
+-- DROP TABLE IF EXISTS public.lut_location CASCADE;
+CREATE TABLE public.lut_location (
+	location_id smallint NOT NULL,
+	area text NOT NULL,
+	track text NOT NULL,
+	CONSTRAINT lut_location_pk PRIMARY KEY (location_id)
+);
+-- ddl-end --
+ALTER TABLE public.lut_location OWNER TO electric;
+-- ddl-end --
+
+INSERT INTO public.lut_location (location_id, area, track) VALUES (E'0', E'Other', E'Other');
+-- ddl-end --
+INSERT INTO public.lut_location (location_id, area, track) VALUES (E'1', E'Pickle', E'Innovation Blvd');
+-- ddl-end --
+INSERT INTO public.lut_location (location_id, area, track) VALUES (E'2', E'Pickle', E'North Lot');
+-- ddl-end --
+INSERT INTO public.lut_location (location_id, area, track) VALUES (E'3', E'Pickle', E'South Lot');
+-- ddl-end --
+INSERT INTO public.lut_location (location_id, area, track) VALUES (E'4', E'COTA', E'Lot J');
+-- ddl-end --
+INSERT INTO public.lut_location (location_id, area, track) VALUES (E'5', E'COTA', E'Lot H');
+-- ddl-end --
+INSERT INTO public.lut_location (location_id, area, track) VALUES (E'6', E'COTA', E'Go Kart Track');
+-- ddl-end --
+
+-- object: public.lut_car | type: TABLE --
+-- DROP TABLE IF EXISTS public.lut_car CASCADE;
+CREATE TABLE public.lut_car (
+	car_id smallint NOT NULL,
+	car_name text NOT NULL,
+	CONSTRAINT lut_car_pk PRIMARY KEY (car_id)
+);
+-- ddl-end --
+ALTER TABLE public.lut_car OWNER TO electric;
+-- ddl-end --
+
+INSERT INTO public.lut_car (car_id, car_name) VALUES (E'1', E'Easy Driver');
+-- ddl-end --
+INSERT INTO public.lut_car (car_id, car_name) VALUES (E'2', E'Lady Luck');
+-- ddl-end --
+
+-- object: public.lut_event_type | type: TABLE --
+-- DROP TABLE IF EXISTS public.lut_event_type CASCADE;
+CREATE TABLE public.lut_event_type (
+	type_id smallint NOT NULL,
+	event_type text NOT NULL,
+	CONSTRAINT lut_event_type_pk PRIMARY KEY (type_id)
+);
+-- ddl-end --
+ALTER TABLE public.lut_event_type OWNER TO electric;
+-- ddl-end --
+
+INSERT INTO public.lut_event_type (type_id, event_type) VALUES (E'0', E'Other');
+-- ddl-end --
+INSERT INTO public.lut_event_type (type_id, event_type) VALUES (E'1', E'Endurance');
+-- ddl-end --
+INSERT INTO public.lut_event_type (type_id, event_type) VALUES (E'2', E'Autocross');
+-- ddl-end --
+INSERT INTO public.lut_event_type (type_id, event_type) VALUES (E'3', E'Skidpad');
+-- ddl-end --
+INSERT INTO public.lut_event_type (type_id, event_type) VALUES (E'4', E'Straightline Acceleration');
+-- ddl-end --
+INSERT INTO public.lut_event_type (type_id, event_type) VALUES (E'5', E'Straightline Breaking');
+-- ddl-end --
+
+-- object: public.event | type: TABLE --
+-- DROP TABLE IF EXISTS public.event CASCADE;
+CREATE TABLE public.event (
+	event_id smallint NOT NULL,
+	day_id smallint NOT NULL,
+	creation_time bigint NOT NULL,
+	start_time bigint,
+	end_time bigint,
+	car_id smallint NOT NULL,
+	driver_id smallint NOT NULL,
+	location_id smallint NOT NULL,
+	event_type smallint NOT NULL,
+	event_index smallint,
+	car_weight smallint,
+	tow_angle real,
+	camber real,
+	ride_height real,
+	ackerman_adjustment real,
+	power_output integer,
+	torque_output smallint,
+	shock_dampening smallint,
+	fr_pressure real,
+	fl_pressure real,
+	br_pressure real,
+	bl_pressure real,
+	front_wing_on boolean,
+	regen_on boolean,
+	rear_wing_on boolean,
+	undertray_on boolean,
+	CONSTRAINT event_pk PRIMARY KEY (event_id)
+);
+-- ddl-end --
+ALTER TABLE public.event OWNER TO electric;
+-- ddl-end --
+
+-- object: public.dynamics | type: TABLE --
+-- DROP TABLE IF EXISTS public.dynamics CASCADE;
+CREATE TABLE public.dynamics (
+	"time" bigint NOT NULL,
+	frw_acc double precision[],
+	flw_acc double precision[],
+	brw_acc double precision[],
+	blw_acc double precision[],
+	body1_acc double precision[],
+	body1_ang double precision[],
+	body2_ang double precision[],
+	body2_acc double precision[],
+	body3_acc double precision[],
+	body3_ang double precision[],
+	accel_pedal_pos real,
+	brake_pressure real,
+	motor_rpm integer,
+	torque_command smallint,
+	gps point
+
+);
+-- ddl-end --
+ALTER TABLE public.dynamics OWNER TO electric;
+-- ddl-end --
+
+-- object: public.power | type: TABLE --
+-- DROP TABLE IF EXISTS public.power CASCADE;
+CREATE TABLE public.power (
+	"time" bigint NOT NULL,
+	bms_cells_v real[],
+	pack_voltage real,
+	pack_current real,
+	lv_cells_v real[],
+	lv_voltage real,
+	lv_current real,
+	ambient_temp real,
+	bms_pack_temp jsonb[],
+	bms_balancing_temp jsonb[],
+	fan_speed jsonb[],
+	inline_cooling_temp real,
+	cooling_flow real
+
+);
+-- ddl-end --
+ALTER TABLE public.power OWNER TO electric;
+-- ddl-end --
+
+-- object: public.electronics | type: TABLE --
+-- DROP TABLE IF EXISTS public.electronics CASCADE;
+CREATE TABLE public.electronics (
+	"time" bigint NOT NULL,
+	imd_on boolean,
+	hv_contactor_on boolean,
+	pre_c_contactor_on boolean,
+	ls_contactor_on boolean,
+	lv_battery_status smallint
+
+);
+-- ddl-end --
+ALTER TABLE public.electronics OWNER TO electric;
+-- ddl-end --
+
+-- object: "FK_day_id" | type: CONSTRAINT --
+-- ALTER TABLE public.event DROP CONSTRAINT IF EXISTS "FK_day_id" CASCADE;
+ALTER TABLE public.event ADD CONSTRAINT "FK_day_id" FOREIGN KEY (day_id)
+REFERENCES public.drive_day (day_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: "FK_car_id" | type: CONSTRAINT --
+-- ALTER TABLE public.event DROP CONSTRAINT IF EXISTS "FK_car_id" CASCADE;
+ALTER TABLE public.event ADD CONSTRAINT "FK_car_id" FOREIGN KEY (car_id)
+REFERENCES public.lut_car (car_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: "FK_driver_id" | type: CONSTRAINT --
+-- ALTER TABLE public.event DROP CONSTRAINT IF EXISTS "FK_driver_id" CASCADE;
+ALTER TABLE public.event ADD CONSTRAINT "FK_driver_id" FOREIGN KEY (driver_id)
+REFERENCES public.lut_driver (driver_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: "FK_location_id" | type: CONSTRAINT --
+-- ALTER TABLE public.event DROP CONSTRAINT IF EXISTS "FK_location_id" CASCADE;
+ALTER TABLE public.event ADD CONSTRAINT "FK_location_id" FOREIGN KEY (location_id)
+REFERENCES public.lut_location (location_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: "FK_event_type" | type: CONSTRAINT --
+-- ALTER TABLE public.event DROP CONSTRAINT IF EXISTS "FK_event_type" CASCADE;
+ALTER TABLE public.event ADD CONSTRAINT "FK_event_type" FOREIGN KEY (event_type)
+REFERENCES public.lut_event_type (type_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+
