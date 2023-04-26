fc2e37933938dad68e6a204215ca610e211df72b
Max Wiesenfeld <maw5859@utexas.edu>
2023-04-25 20:41:17 -0500

    Completed V2.0 of DB

--- stack/db_init_v2.0.sql
+++ stack/db_init_v2.0.sql
@@ -43,10 +43,35 @@ CREATE ROLE analysis WITH
 -- ddl-end --
 
 
+SET check_function_bodies = false;
+-- ddl-end --
+
+-- object: public.get_event_index | type: FUNCTION --
+-- DROP FUNCTION IF EXISTS public.get_event_index(smallint,smallint) CASCADE;
+CREATE OR REPLACE FUNCTION public.get_event_index (car smallint, day smallint)
+	RETURNS smallint
+	LANGUAGE plpgsql
+	IMMUTABLE
+	STRICT
+	AS
+$$
+DECLARE ind smallint;
+BEGIN
+	SELECT COUNT(event_id)
+	INTO ind
+	FROM event
+	WHERE car_id = car AND day_id = day;
+	RETURN ind + 1;
+END;
+$$;
+-- ddl-end --
+ALTER FUNCTION public.get_event_index(smallint,smallint) OWNER TO electric;
+-- ddl-end --
+
 -- object: public.drive_day | type: TABLE --
 -- DROP TABLE IF EXISTS public.drive_day CASCADE;
 CREATE TABLE public.drive_day (
-	day_id smallint NOT NULL,
+	day_id smallserial NOT NULL,
 	date date NOT NULL,
 	power_limit integer,
 	conditions text,
@@ -151,7 +176,7 @@ INSERT INTO public.lut_event_type (type_id, event_type) VALUES (E'5', E'Straight
 -- object: public.event | type: TABLE --
 -- DROP TABLE IF EXISTS public.event CASCADE;
 CREATE TABLE public.event (
-	event_id smallint NOT NULL,
+	event_id smallserial NOT NULL,
 	day_id smallint NOT NULL,
 	creation_time bigint NOT NULL,
 	start_time bigint,
@@ -160,22 +185,22 @@ CREATE TABLE public.event (
 	driver_id smallint NOT NULL,
 	location_id smallint NOT NULL,
 	event_type smallint NOT NULL,
-	event_index smallint,
+	event_index smallint GENERATED ALWAYS AS (public.get_event_index(car_id, day_id)) STORED,
 	car_weight smallint,
 	tow_angle real,
 	camber real,
 	ride_height real,
 	ackerman_adjustment real,
-	power_output integer,
-	torque_output smallint,
 	shock_dampening smallint,
+	power_limit integer,
+	torque_limit smallint,
 	fr_pressure real,
 	fl_pressure real,
 	br_pressure real,
 	bl_pressure real,
 	front_wing_on boolean,
-	regen_on boolean,
 	rear_wing_on boolean,
+	regen_on boolean,
 	undertray_on boolean,
 	CONSTRAINT event_pk PRIMARY KEY (event_id)
 );
@@ -244,6 +269,7 @@ CREATE TABLE public.electronics (
 -- ddl-end --
 ALTER TABLE public.electronics OWNER TO electric;
 -- ddl-end --
+
 
 -- object: "FK_day_id" | type: CONSTRAINT --
 -- ALTER TABLE public.event DROP CONSTRAINT IF EXISTS "FK_day_id" CASCADE;
