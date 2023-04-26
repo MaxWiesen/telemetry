8a335f4453d7dc175f9536d7977545ab03d04637
Max Wiesenfeld <maw5859@utexas.edu>
2023-04-25 20:43:09 -0500

    Completed routing and error checks

--- analysis/database/server/viewer_tool/index.py
+++ analysis/database/server/viewer_tool/index.py
@@ -1,5 +1,3 @@
-import time
-
 from flask import Flask, render_template, url_for, request, redirect
 
 from analysis.database.server.one_time_injection import Injector
@@ -15,43 +13,32 @@ def index():
 
 @app.route('/new_drive_day/', methods=['GET'])
 def new_drive_day():
-    if request.method == 'GET':
-        day_id = Injector.create_drive_day(**request.args)
-        return redirect(url_for('.new_event', day_id=day_id, method='new'))
-    else:
-        return 'How did you even get here?'
+    day_id = Injector.create_from_table(table='drive_day', user='electric', data=request.args)
+    return redirect(url_for('.new_event', day_id=day_id, method='new'))
 
 
 @app.route('/new_event/', methods=['GET'])
 def new_event():
-    cnx = DBHandler().connect()
-    try:
-        cur = cnx.cursor()
-        cur.execute(f'SELECT day_id WHERE day_id = {request.args["day_id"]}')
-        row = cur.fetchone()
-        if not row and request.args['method'] == 'existing':
-            return 'Drive Day ID not found in database. Try again.' + render_template('index.html')
-        elif not row and request.args['method'] == 'new':
-            return 'Error: Attempt to create new drive day failed. ID not found after creation. Inform developer.'
-    except Exception as e:
-        raise e
-    finally:
-        DBHandler.kill(cnx)
-    return render_template('input_screen.html')
-
-# @app.route('/create_event/', methods=['POST'])
-# def create_event():
+    with DBHandler().connect(user='electric') as cnx:
+        day_id = request.form.get('day_id', request.args['day_id'])
+        with cnx.cursor() as cur:
+            cur.execute(f'SELECT day_id FROM drive_day WHERE day_id = {day_id}')
+            row = cur.fetchone()
+    if not row and request.args['method'] == 'existing':
+        return 'Drive Day ID not found in database. Try again.' + render_template('index.html')
+    elif not row and request.args['method'] == 'new':
+        return 'Error: Attempt to create new drive day failed. ID not found after creation. Inform developer.'
+    return render_template('input_screen.html', day_id=day_id)
 
 
-# @app.route('/dev/', methods=['GET', 'POST'])
-# def dev_index():
-#     if request.method == 'POST':
-#         inputs = request.form
-#         print(inputs)
-#         return 'On Dev'
-#     else:
-#         return render_template('dev.html')
+@app.route('/create_event/', methods=['POST'])
+def create_event():
+    event_id = Injector.create_from_table(table='event', user='electric', data=request.form)
+    return render_template('event_tracker.html')
 
+@app.route('/start_event/', methods=['POST'])
+def start_event():
+    status = Injector.inject(table='event', user='electric')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
