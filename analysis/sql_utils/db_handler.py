c0bac1e09b9a4d885cdfac2897367577fe04c4a8
Max Wiesenfeld <maw5859@utexas.edu>
2023-05-03 15:01:30 -0500

    Began automated table description

--- analysis/sql_utils/db_handler.py
+++ analysis/sql_utils/db_handler.py
@@ -1,22 +1,22 @@
 import psycopg
-import logging
+import pickle
 import time
 import numpy as np
 import datetime
 
 
 class TableSpecs:
-    table_column_specs = {  # Last Updated for DB schema v1.2
+    # TODO 
+    table_column_specs = {  # Last Updated for DB schema v2.0
         'drive_day': {
             'day_id': {'type': int, 'range': (0, 1_000)},
-            'conditions': {'type': str},
+            'date': {'type': datetime.date, 'range': (datetime.date.today().isoformat(), datetime.date.today().isoformat())},
             'power_limit': {'type': int, 'range': (0, 80_000)},
-            'date': {'type': datetime.datetime, 'range': (datetime.date.today().isoformat(), datetime.date.today().isoformat())},
-
+            'conditions': {'type': str}
         },
         'event': {
             'event_id': {'type': int, 'range': (0, 1_000)},
-            'drive_day': {'type': int, 'range': (0, 1_000)},
+            'day_id': {'type': int, 'range': (0, 1_000)},
             'creation_time': {'type': float, 'range': (time.time(), time.time())},
             'start_time': {'type': float, 'range': (time.time(), time.time())},
             'end_time': {'type': float, 'range': (time.time(), time.time())},
@@ -30,13 +30,13 @@ class TableSpecs:
             'camber': {'type': np.float32, 'range': (0, 0)},
             'ride_height': {'type': np.float32, 'range': (0, 0)},
             'ackerman_adjustment': {'type': np.float32, 'range': (0, 0)},
-            'power_output': {'type': int, 'range': ()},
-            'torque_output':  {'type': int, 'range': ()},
             'shock_dampening': {'type': int, 'range': ()},
-            'frw_pressure': {'type': float, 'range': (0, 100)},
-            'flw_pressure': {'type': float, 'range': (0, 100)},
-            'brw_pressure': {'type': float, 'range': (0, 100)},
-            'blw_pressure': {'type': float, 'range': (0, 100)},
+            'power_limit': {'type': int, 'range': ()},
+            'torque_limit': {'type': int, 'range': ()},
+            'fr_pressure': {'type': float, 'range': (0, 100)},
+            'fl_pressure': {'type': float, 'range': (0, 100)},
+            'br_pressure': {'type': float, 'range': (0, 100)},
+            'bl_pressure': {'type': float, 'range': (0, 100)},
             'front_wing_on': {'type': bool},
             'rear_wing_on': {'type': bool},
             'regen_on': {'type': bool},
@@ -84,6 +84,28 @@ class TableSpecs:
             'lv_battery_status': {'type': int, 'range': (0, 5)}
         }
     }
+    def __init__(self, verbose_output=False, force=False):
+        # now = time.time()
+        # time, old_specs = pickle.load(open('DB_description.pkl', 'rb'))
+        # if force or now - time > 86_400 * 10:    # Days since last update
+        #     self.update_table_column_specs()
+        #     pickle.dump((now, self.table_column_specs), open('DB_description.pkl', 'wb'))
+        self.update_table_column_specs()
+
+
+    def update_table_column_specs(self):
+        with DBHandler().connect(user='electric') as cnx:
+            with cnx.cursor() as cur:
+                cur.execute("""SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+                               WHERE TABLE_NAME LIKE '%%' AND TABLE_SCHEMA = 'public'""")
+                # Potential query to avoid multiple IO operations
+                # select
+                # TABLE_NAME, COLUMN_NAME, DATA_TYPE
+                # from INFORMATION_SCHEMA.COLUMNS WHERE
+                # TABLE_SCHEMA = 'public'
+                tables = [elem[0] for elem in cur.fetchall()]
+                for table in tables:
+                    cur.execute(f'')
 
 
 class DBHandler:
@@ -110,24 +132,9 @@ class DBHandler:
         return psycopg.connect(dbname=config['dbname'], user=user, password=config['user'][user],
                                host=config['host'], port=config['port'])
 
-    @staticmethod
-    def kill(cnx, commit=True):
-        if commit:
-            cnx.commit()
-        if not cnx.closed:
-            cnx.close()
-        else:
-            logging.info('An attempt was made to kill a closed connection.')
-
 
 if __name__ == '__main__':
-    try:
-        handler = DBHandler()
-        cnx = handler.connect(user='electric')
-        with cnx.cursor() as cur:
-            cur.execute(f"INSERT INTO event (creation_time) VALUES ('{datetime.datetime.now()}') RETURNING event_id")
-            print(cur.fetchone())
-    except Exception as e:
-        raise e
-    finally:
-        DBHandler.kill(cnx)
+    TableSpecs()
+    # with DBHandler.connect(user='electric') as cnx:
+    #     with cnx.cursor() as cur:
+    #         cur.execute('query')
