2e94441ffbdbd804d4522281f72ae78928b01b5a
Max Wiesenfeld <maw5859@utexas.edu>
2023-05-03 15:03:33 -0500

    Added event time setter

--- analysis/database/one_time_injection.py
+++ analysis/database/one_time_injection.py
@@ -1,21 +1,22 @@
 import datetime
 import time
 import logging
-from analysis.database.sql_utils.db_handler import DBHandler, TableSpecs
+from analysis.sql_utils.db_handler import DBHandler, TableSpecs
 
 
 class Injector:
     # def __init__(self, **kwargs):
     @staticmethod
     def get_insert_values(table, data, skip_first=True):
+        # Gather expected table columns
+        table_cols = list(TableSpecs.table_column_specs[table].items())[skip_first:]
+
         # Individual table modifications
         if table == 'drive_day':
             data['date'] = datetime.date.today().isoformat()
         elif table == 'event':
             data['creation_time'] = int(time.time() * 1000)
-
-        # Gather expected table columns
-        table_cols = list(TableSpecs.table_column_specs[table].items())[int(skip_first):]
+            table_cols = list(filter(lambda col: col[0] != 'event_index', table_cols))
 
         # Find columns missing from Flask app injection
         missing_cols = [col for col, _ in table_cols if col not in data]
@@ -23,7 +24,7 @@ class Injector:
             logging.warning(f'\t\tFollowing columns were missing from transmitted data: {", ".join(missing_cols)}')
 
         # Separate NaNs and log
-        nan_vals = [val == 0 or bool(val) for key, val in data.items()]
+        nan_vals = [val == 0 or bool(val) for _, val in data.items()]
         nans = {key: val for (key, val), nan in zip(data.items(), nan_vals) if not nan}
         data = {key: val for (key, val), nan in zip(data.items(), nan_vals) if nan}
         if nans:
@@ -32,7 +33,7 @@ class Injector:
         return ', '.join(data.keys()), list(data.values())
 
     @classmethod
-    def create_from_table(cls, table, user='analysis', data=None):
+    def insert(cls, table, user='analysis', data=None):
         if data is None:
             raise ValueError('No data in payload.')
 
@@ -40,9 +41,21 @@ class Injector:
 
         with DBHandler().connect(user=user) as cnx:
             with cnx.cursor() as cur:
-                cur.execute(f'''INSERT INTO {table} ({cols}) VALUES (%s{', %s' * (len(vals) - 1)}) 
+                cur.execute(f'''INSERT INTO {table} ({cols})
+                                VALUES (%s{', %s' * (len(vals) - 1)}) 
                                 RETURNING {list(TableSpecs.table_column_specs[table].keys())[0]}''', vals)
                 return cur.fetchone()[0]
+
+    @classmethod
+    def set_event_time(cls, event_id, user='analysis', start=True):
+        now = int(time.time() * 1000)
+        with DBHandler().connect(user=user) as cnx:
+            with cnx.cursor() as cur:
+                cur.execute(f'''UPDATE event SET {'start' if start else 'stop'}_time = {now}
+                                WHERE event_id = {event_id}
+                                RETURNING {'start' if start else 'stop'}_time''')
+                return cur.fetchone()[0] == now
+
 
 
 if __name__ == '__main__':
