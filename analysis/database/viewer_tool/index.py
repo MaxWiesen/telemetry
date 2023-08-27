a4ce8626f4a71bc333e36f3166961739bbcaa3ac
Max Wiesenfeld <maw5859@utexas.edu>
2023-08-27 11:04:04 -0500

    Mosquitto connection and event ID persistence

--- analysis/database/viewer_tool/index.py
+++ analysis/database/viewer_tool/index.py
@@ -1,11 +1,14 @@
+import json
 import logging
+import sys
+sys.path.append('~/Documents/LHR/stack')
 
 from flask import Flask, render_template, url_for, request, redirect
-
-from analysis.database.one_time_injection import Injector
 from analysis.sql_utils.db_handler import DBHandler
+from stack.ingest.mqtt_handler import mosquitto_connect
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -14,7 +17,7 @@ def index():
 
 @app.route('/new_drive_day/', methods=['GET'])
 def new_drive_day():
-    day_id = Injector.insert(table='drive_day', user='electric', data=request.args)
+    day_id = DBHandler.insert(table='drive_day', user='electric', data=request.args)
     return redirect(url_for('.new_event', day_id=day_id, method='new'))
 
 
@@ -25,6 +28,8 @@ def new_event():
         with cnx.cursor() as cur:
             cur.execute(f'SELECT day_id FROM drive_day WHERE day_id = {day_id}')
             row = cur.fetchone()
+    client = mosquitto_connect()
+    client.publish('flask', json.dumps({'event_id': day_id}, indent=2))
     if not row and request.args['method'] == 'existing':
         return 'Drive Day ID not found in database. Try again.' + render_template('index.html')
     elif not row and request.args['method'] == 'new':
@@ -34,15 +39,15 @@ def new_event():
 
 @app.route('/create_event/', methods=['POST'])
 def create_event():
-    event_id = Injector.insert(table='event', user='electric', data=request.form)
+    event_id = DBHandler.insert(table='event', user='electric', data=request.form)
     return render_template('event_tracker.html', event_id=event_id)
 
 
 @app.route('/set_event_time/', methods=['POST'])
 def set_event_time():
-    if Injector.set_event_time(event_id := request.form['event_id'], 'electric', 'start' in request.form):
+    if DBHandler.set_event_time(event_id := request.form['event_id'], 'electric', 'start' in request.form):
         return render_template('event_tracker.html', event_id=event_id)
-    logging.error('\t\tset_event_time FAILURE: Value written to database not equal to time created.')
+    logging.warning('\t\tset_event_time FAILURE: Value written to database not equal to time created.')
     return 'Error setting time. Please contact a dev or try again:\n' + render_template('index.html')
 
 
