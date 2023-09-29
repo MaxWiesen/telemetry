5bedc0b479ff9c50a2c85d883cb50ed91b43a158
Max Wiesenfeld <maw5859@utexas.edu>
2023-09-29 13:32:06 -0500

    First commit

--- stack/ingest/mqtt_handler.py
+++ stack/ingest/mqtt_handler.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import json
+
 from paho.mqtt import client as mqtt_client
 if os.getenv('IN_DOCKER'):
     from db_handler import DBHandler, get_table_column_specs    # Cheesed import statement using bind mount
@@ -11,7 +12,7 @@ else:
 def mosquitto_connect():
     client = mqtt_client.Client('python_client')
     client.on_connect = lambda clients, userdata, flags, rc: logging.error(f'Failed to connect to Mosquitto Broker, return code {rc}\n') if rc \
-        else logging.info('Python Client connected to Moquitto Broker')
+        else logging.info('\t\tPython Client connected to Mosquitto Broker')
     client.connect('mosquitto' if os.getenv('IN_DOCKER') else 'localhost')
     return client
 
@@ -20,16 +21,15 @@ def main():
     client = mosquitto_connect()
 
     def on_message(clients, userdata, msg):
+        logging.info('\n\n\n\n\nOn_message')
         table = msg.topic.rsplit('/', 1)[-1]
         payload = json.loads(msg.payload.decode())
-        logging.info(table in get_table_column_specs())
-        logging.info(table)
-        logging.info(get_table_column_specs())
         if table == 'flask':
             if (event_id := payload.get('event_id')):
+                logging.info(f'Now logging data for event: {event_id}...')
                 os.environ['EVENT_ID'] = event_id
-        # if table == 'start' or table == 'stop':
-        #     DBHandler.set_event_time()
+            # elif payload.get('stop_event'):
+            #     client.loop_stop()
         elif table in get_table_column_specs():
             logging.info(f'Data received for {table}. Inserting to Database now...')
             DBHandler.insert(table, user='electric', data=payload)
