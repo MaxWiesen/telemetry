5639ee69744408f2be90d57d18fd91706e9e2153
Max Wiesenfeld <maw5859@utexas.edu>
2023-10-15 16:28:08 -0500

    Adapted for set_event_time returning functionality

--- analysis/database/viewer_tool/index.py
+++ analysis/database/viewer_tool/index.py
@@ -24,16 +24,7 @@ def new_drive_day():
 
 @app.route('/new_event/', methods=['GET'])
 def new_event():
-    with DBHandler().connect(user='electric') as cnx:
-        day_id = request.form.get('day_id', request.args['day_id'])
-        with cnx.cursor() as cur:
-            cur.execute(f'SELECT day_id FROM drive_day WHERE day_id = {day_id}')
-            row = cur.fetchone()[0]
-    if not row and request.args['method'] == 'existing':
-        return 'Drive Day ID not found in database. Try again.' + render_template('index.html')
-    elif not row and request.args['method'] == 'new':
-        return 'Error: Attempt to create new drive day failed. ID not found after creation. Inform developer.'
-    return render_template('input_screen.html', day_id=day_id)
+    return render_template('input_screen.html', day_id=request.form.get('day_id', request.args['day_id']))
 
 
 @app.route('/create_event/', methods=['POST'])
@@ -46,13 +37,12 @@ def create_event():
 
 @app.route('/set_event_time/', methods=['POST'])
 def set_event_time():
-    if DBHandler.set_event_time(event_id := request.form['event_id'], 'electric', 'start' in request.form):
-        if 'stop' in request.form:
-            client = mosquitto_connect()
-            client.publish('flask', json.dumps({'stop_event': True}, indent=4))
-        return render_template('event_tracker.html', event_id=event_id)
-    logging.warning('\t\tset_event_time FAILURE: Value written to database not equal to time created.')
-    return 'Error setting time. Please contact a dev or try again:\n' + render_template('index.html')
+    event_id = int(request.form['event_id'])
+    day_id = DBHandler.set_event_time(event_id, 'electric', 'start' in request.form, 'day_id')
+    if 'start' not in request.form:
+        client = mosquitto_connect()
+        client.publish('flask', json.dumps({'end_event': True}, indent=4))
+    return render_template('event_tracker.html', event_id=event_id)
 
 
 if __name__ == '__main__':
