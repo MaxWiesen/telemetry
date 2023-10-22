55f7e89b93a4259bf82f22bfe4bb08e68cb4eca1
Max Wiesenfeld <maw5859@utexas.edu>
2023-10-22 15:08:44 -0500

    Documented functions and fixed some DB_description issues

--- analysis/sql_utils/db_handler.py
+++ analysis/sql_utils/db_handler.py
@@ -8,6 +8,15 @@ import os
 
 
 def get_table_column_specs(force=False, verbose=False):
+    """
+    Gets description of DB layout using either recent pkl file or request to database. Returns description in form of
+    dict as follows: {'power': [('cooling_flow', <class 'float'>), (col2, type2), ...], table2: [...]}
+
+    :param force:           bool determines whether or not to force refresh of cached description (pkl file)
+    :param verbose:         bool used to pretty print most updated DB layout, only works if debugging level includes info
+
+    :return db_description: dict represents current layout of DB--see function description for more explanation
+    """
     #TODO: 1) Find underlying data types of ARRAY; 2) Add point type compatibility
     desc_path = './DB_description.pkl' if os.getenv('IN_DOCKER') else os.getcwd().rsplit('analysis/', 1)[0] + '/analysis/sql_utils/DB_description.pkl'
     if os.path.isfile(desc_path):
@@ -18,21 +27,19 @@ def get_table_column_specs(force=False, verbose=False):
     if not os.getenv('IN_DOCKER'):
         now = time.time()
         if force or now - last_update > 86_400 * 1:     # Days since last update
-            with DBHandler().connect(user='electric') as cnx:
-                with cnx.cursor() as cur:
-                    cur.execute("""SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE 
-                                   FROM INFORMATION_SCHEMA.COLUMNS 
-                                   WHERE TABLE_SCHEMA = 'public'""")
-                    data = pd.DataFrame(cur.fetchall(), columns=['table_name', 'column_name', 'data_type'])
-                    data.data_type.replace({'smallint': int, 'integer': int, 'bigint': int, 'real': float, 'text': str,
-                                            'boolean': bool, 'date': datetime.date, 'ARRAY': list}, inplace=True)
-                    table_column_specs = {table: list(zip((table_data := data[data.table_name == table]).column_name,
-                                                          table_data.data_type)) for table in data.table_name.unique()}
-                pickle.dump((now, table_column_specs), open(desc_path, 'wb'))
-                logging.info(f'\t\ttable_column_specs out of date and updated.')
-                if verbose:     # Pretty print table column specs
-                    [logging.info(f'\t\t{table}\n' + ''.join([f'\n{col:^20} {str(dtype):^30}' for col, dtype in col_data]) +
-                                  '\n') for table, col_data in table_column_specs.items()]
+            data = DBHandler.simple_select("""SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE 
+                                           FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'public'""",
+                                           target='PROD', user='electric', return_type=pd.DataFrame)
+            data.data_type.replace({'smallint': int, 'integer': int, 'bigint': int, 'real': float, 'text': str,
+                                    'boolean': bool, 'date': datetime.date, 'ARRAY': list}, inplace=True)
+            data.loc[data.column_name == 'date', 'data_type'] = str
+            table_column_specs = {table: list(zip((table_data := data[data.table_name == table]).column_name,
+                                                  table_data.data_type)) for table in data.table_name.unique()}
+            pickle.dump((now, table_column_specs), open(desc_path, 'wb'))
+            logging.info(f'\t\ttable_column_specs out of date and updated.')
+        if verbose:     # Pretty print table column specs
+            [logging.info(f'\t\t{table}\n' + ''.join([f'\n{col:^20} {str(dtype):^30}' for col, dtype in col_data]) +
+                          '\n') for table, col_data in table_column_specs.items()]
     return table_column_specs
 
 
@@ -70,6 +77,17 @@ class DBHandler:
 
     @classmethod
     def simple_select(cls, query: str, target='PROD', user='electric', handler=None, return_type=None):
+        """
+        Simple, easy way to get data from database. ONLY USED FOR SELECTING
+
+        :param query:       str SQL query to send to database
+        :param target:      str indicating target database server (according to DB_CONFIG)
+        :param user:        str indicating what user to use to sign in to server
+        :param handler:     DBHandler handler to use to send requests (made and discarded if not given)
+        :param return_type: type used to determine what type to return data in
+
+        :return:            list[tuple] | type returns result of query's fetchall as list of tuple rows or as return_type
+        """
         if handler is None:
             handler = cls()
 
@@ -110,34 +128,33 @@ class DBHandler:
             data['creation_time'] = int(time.time() * 1000)
             table_cols = list(filter(lambda col: col[0] not in ['event_index', 'event_id'], table_cols))
 
-        if table in ['electronics', 'dynamics', 'power']:
-            data['event_id'] = int(os.getenv('EVENT_ID'))
-
         # Find columns missing from Flask app injection
         missing_cols = [col for col, _ in table_cols if col not in data]
         if missing_cols:
             logging.warning(f'\t\tFollowing columns were missing from transmitted data: {", ".join(missing_cols)}')
 
         # Separate NaNs and log
+        table_cols = dict(table_cols)
         nan_vals = [val == 0 or bool(val) for _, val in data.items()]
         nans = {key: val for (key, val), nan in zip(data.items(), nan_vals) if not nan}
-        data = {key: val for (key, val), nan in zip(data.items(), nan_vals) if nan}
+        data = {key: table_cols[key](val) for (key, val), nan in zip(data.items(), nan_vals) if nan}
         if nans:
             logging.warning(f'\t\tFollowing columns had NaN data: {str(nans).replace(": ", " = ")[1:-1]}')
 
         return data
 
     @classmethod
-    def insert(cls, table, user='analysis', data=None, returning=None):
+    def insert(cls, table, target='PROD', user='analysis', data=None, returning=None):
         """
         Targets a table and sends an individual row of data to database, with ability to get columns from the last row.
 
         :param table:       str indicating which table the insertion is targeting
+        :param target:      str indicating target database server (according to DB_CONFIG)
         :param user:        str indicating what user to use to sign in to server
         :param data:        dict | request.* holds data to send to database
         :param returning:   str | list column names to return values for after request is executed
 
-        :return data:       dict containing preprocessed column name and values
+        :return data:       SQL_VALUE | tuple of values in order of returning
         """
         if data is None:
             raise ValueError('No data in payload.')
@@ -147,7 +164,7 @@ class DBHandler:
 
         data = cls.get_insert_values(table, dict(data))
 
-        with DBHandler().connect(user=user) as cnx:
+        with DBHandler().connect(target, user) as cnx:
             with cnx.cursor() as cur:
                 cur.execute(f'''INSERT INTO {table} ({', '.join(data.keys())})
                                     VALUES (%s{', %s' * (len(vals:=list(data.values())) - 1)}) 
@@ -156,9 +173,20 @@ class DBHandler:
                 return cur.fetchone()[0] if isinstance(returning, str) else cur.fetchone()
 
     @classmethod
-    def set_event_time(cls, event_id, user='analysis', start=True, returning=None):
+    def set_event_time(cls, event_id, target='PROD', user='analysis', start=True, returning=None):
+        """
+        Targets an event_id and updates the start or end time, with ability to get columns from the affected row.
+
+        :param event_id:    int event_id whose start/end time will be updated
+        :param target:      str indicating target database server (according to DB_CONFIG)
+        :param user:        str indicating what user to use to sign in to server
+        :param start:       bool indicates whether to set start or end time
+        :param returning:   str | list column names to return values for after request is executed
+
+        :return data:       SQL_VALUE | tuple of values in order of returning
+        """
         now = int(time.time() * 1000)
-        with DBHandler().connect(user=user) as cnx:
+        with DBHandler().connect(target, user) as cnx:
             with cnx.cursor() as cur:
                 cur.execute(f'''UPDATE event SET {'start' if start else 'end'}_time = {now}
                                     WHERE event_id = {event_id}
