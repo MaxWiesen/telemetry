ca30b7b6546f8681dc349cf17c4f389d35c19639
Max Wiesenfeld <maw5859@utexas.edu>
2023-10-22 15:10:46 -0500

    Added Flask -> MQTT transactions

--- analysis/database/viewer_tool/index.py
+++ analysis/database/viewer_tool/index.py
@@ -18,7 +18,7 @@ def index():
 
 @app.route('/new_drive_day/', methods=['GET'])
 def new_drive_day():
-    day_id = DBHandler.insert(table='drive_day', user='electric', data=request.args, returning='day_id')
+    day_id = DBHandler.insert(table='drive_day', target='PROD', user='electric', data=request.args, returning='day_id')
     return redirect(url_for('.new_event', day_id=day_id, method='new'))
 
 
@@ -29,19 +29,19 @@ def new_event():
 
 @app.route('/create_event/', methods=['POST'])
 def create_event():
-    day_id, event_id = DBHandler.insert(table='event', user='electric', data=request.form, returning=['day_id', 'event_id'])
-    client = mosquitto_connect()
-    client.publish('flask', json.dumps({'event_id': event_id}, indent=4))
+    day_id, event_id = DBHandler.insert(table='event', target='PROD', user='electric', data=request.form, returning=['day_id', 'event_id'])
+    client = mosquitto_connect('flask_sender')
+    client.publish('config/flask', json.dumps({'event_id': event_id}, indent=4))
     return render_template('event_tracker.html', event_id=event_id)
 
 
 @app.route('/set_event_time/', methods=['POST'])
 def set_event_time():
     event_id = int(request.form['event_id'])
-    day_id = DBHandler.set_event_time(event_id, 'electric', 'start' in request.form, 'day_id')
+    day_id = DBHandler.set_event_time(event_id, 'PROD', 'electric', 'start' in request.form, 'day_id')
     if 'start' not in request.form:
-        client = mosquitto_connect()
-        client.publish('flask', json.dumps({'end_event': True}, indent=4))
+        client = mosquitto_connect('flask_sender')
+        client.publish('config/flask', json.dumps({'end_event': True}, indent=4))
     return render_template('event_tracker.html', event_id=event_id)
 
 
