4a69b1b3c8b4192966681b054eb49fbd11791e69
Max Wiesenfeld <maw5859@utexas.edu>
2024-04-14 18:17:17 -0500

    Added event status and dynamic IP targeting

--- analysis/database/viewer_tool/index.py
+++ analysis/database/viewer_tool/index.py
@@ -1,5 +1,5 @@
-import logging
 import json
+import os
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parents[3]))
@@ -9,7 +9,6 @@ from analysis.sql_utils.db_handler import DBHandler
 from stack.ingest.mqtt_handler import MQTTHandler
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -29,24 +28,25 @@ def new_event():
 
 @app.route('/create_event/', methods=['POST'])
 def create_event():
-    day_id, event_id = DBHandler.insert(table='event', target='PROD', user='electric', data=request.form, returning=['day_id', 'event_id'])
-    mqtt = MQTTHandler()
+    inputs = request.form.to_dict()
+    inputs['status'] = 2
+    day_id, event_id = DBHandler.insert(table='event', target='PROD', user='electric', data=inputs, returning=['day_id', 'event_id'])
+    mqtt = MQTTHandler('flask_app')
     mqtt.connect()
-    mqtt.publish('/config/flask', json.dumps({'event_id': event_id}, indent=4))
+    mqtt.publish('config/flask', json.dumps({'event_id': event_id}, indent=4))
     mqtt.disconnect()
-    return render_template('event_tracker.html', event_id=event_id)
+    return render_template('event_tracker.html', host_ip=os.getenv('HOST_IP', 'localhost'), event_id=event_id)
 
 
 @app.route('/set_event_time/', methods=['POST'])
 def set_event_time():
-    event_id = int(request.form['event_id'])
-    day_id = DBHandler.set_event_time(event_id, 'PROD', 'electric', 'start' in request.form, 'day_id')
-    if 'start' not in request.form:
-        mqtt = MQTTHandler()
+    DBHandler.set_event_status(**request.json, target='PROD', user='electric', returning='day_id')
+    if request.json['status'] == 0:
+        mqtt = MQTTHandler('flask_app')
         mqtt.connect()
-        mqtt.publish('/config/flask', json.dumps({'end_event': True}, indent=4))
+        mqtt.publish('config/flask', 'end_event')
         mqtt.disconnect()
-    return render_template('event_tracker.html', event_id=event_id)
+    return render_template('event_tracker.html', host_ip=os.getenv('HOST_IP', 'localhost'), event_id=request.json.get('event_id'))
 
 
 @app.route('/tune_data', methods=['GET','POST'])
@@ -62,7 +62,7 @@ def turn_data():
     data = request.data
     json_object = json.loads(data)
     print(json_object)
-    return render_template('event_tracker.html')
+    return render_template('event_tracker.html', host_ip=os.getenv('HOST_IP', 'localhost'))
 
 
 @app.route('/accel_data', methods=['GET','POST'])
@@ -70,15 +70,24 @@ def accel_data():
     data = request.data
     json_object = json.loads(data)
     print(json_object)
-    return render_template('event_tracker.html')
+    return render_template('event_tracker.html', host_ip=os.getenv('HOST_IP', 'localhost'))
 
 
 @app.route('/texas_tune/', methods=['GET', 'POST'])
 def vcu_parameters():
-    return render_template('texas_tune.html')
     if request.method == 'POST':
         print(request)
+    elif request.method == 'GET':
+        return render_template('texas_tune.html')
+
+
+@app.route('/gates/', methods=['POST'])
+def create_gates():
+    pass
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', ssl_context=('./ssl/fullchain.pem', './ssl/privkey.pem'))
+    if os.getenv('IN_DOCKER'):
+        app.run(host='0.0.0.0', ssl_context=('./ssl/fullchain.pem', './ssl/privkey.pem'))
+    else:
+        app.run(host='0.0.0.0', debug=True)
