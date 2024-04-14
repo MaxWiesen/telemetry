e1de07a0fb195b64e83d088b788753b0162a6899
Max Wiesenfeld <maw5859@utexas.edu>
2024-04-14 18:11:45 -0500

    Aggregated status and set time and general improvements

--- stack/ingest/mqtt_handler.py
+++ stack/ingest/mqtt_handler.py
@@ -20,6 +20,7 @@ class MQTTHandler:
         :param name:    str determining name of client to self-report to MQTT broker
         '''
         self.client = mqtt_client.Client(name)
+        self.client.username = name
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
         self.client.on_message = self.on_message
@@ -29,23 +30,12 @@ class MQTTHandler:
         if rc:
             logging.error(f'Failed to connect to Mosquitto Broker, return code {rc}\n')
         else:
-            logging.info(f'\t\t{client} connected to Mosquitto Broker')
+            logging.info(f'\t\t{client.username} connected to Mosquitto Broker')
 
     @staticmethod
     def on_disconnect(client: mqtt_client.Client, userdata, rc: int):
         if rc != 0:
             print(f'Unexpected MQTT disconnection. Return code: {rc}')
-
-    def on_message(self, client: mqtt_client.Client, userdata, msg):
-        logging.info(f'Received message at topic {msg.topic}: {msg.payload}')
-        if msg.topic == '/config/flask':
-            self.flask_handler(json.loads(msg.payload.decode()))
-        elif msg.topic == '/config/car':
-            os.environ['RTC_START'] = str(datetime.datetime.strptime(msg.payload.decode(), "%Y-%m-%dT%H:%M:%S.%f").timestamp() * 1000)
-        elif (freq := msg.topic.rsplit('/')[-1]) in ['h', 'l']:
-            self.data_ingester(msg.payload, freq)
-        else:
-            logging.warning(f'No corresponding topic found for {msg.topic}')
 
     def connect(self, ip=None):
         '''
@@ -68,36 +58,62 @@ class MQTTHandler:
     def publish(self, *args, **kwargs):
         self.client.publish(*args, **kwargs)
 
-    def flask_handler(self, payload):
-        if event_id := payload.get('event_id'):
-            logging.info(f'Now logging data for event: {event_id}...')
+    def on_message(self, client: mqtt_client.Client, userdata, msg):
+        logging.info(f'Received message at topic {msg.topic}: {msg.payload}')
+        if msg.topic == 'config/flask':
+            self.__flask_handler(msg.payload.decode())
+        elif msg.topic == 'config/car':
+            os.environ['RTC_START'] = str(datetime.datetime.strptime(msg.payload.decode(), "%Y-%m-%dT%H:%M:%S.%f").timestamp() * 1000)
+        elif (freq := msg.topic.rsplit('/')[-1]) in ['h', 'l']:
+            self.__b64_ingest(msg.payload, freq)
+        elif (topic_split := msg.topic.split('/'))[0] == 'data':
+            self.__data_ingest(msg.payload, topic_split[-1])
+        else:
+            logging.warning(f'No corresponding topic found for {msg.topic}')
+
+    def __flask_handler(self, payload):
+        try:
+            event_id = json.loads(payload)['event_id']
+            logging.info(f'\tNow logging data for event: {event_id}...')
             os.environ['EVENT_ID'] = str(event_id)
+        except json.JSONDecodeError:
+            if payload == 'end_event':
+                event_id = os.environ['EVENT_ID']
+                del os.environ['EVENT_ID']
+                try:
+                    del os.environ['RTC_START']
+                    logging.info(f'\tEnding logging for event {event_id}...')
+                except KeyError:
+                    logging.info(f'\tEnding logging for event {event_id} despite RTC_START never being set...')
+            else:
+                logging.error(f'\tUnexpected payload received: {payload}')
 
-        elif payload.get('end_event'):
-            logging.info(f'Ending logging for event {os.environ["EVENT_ID"]}...')
-            del os.environ['EVENT_ID'], os.environ['RTC_START']
-
-    def data_ingester(self, payload, high_freq: bool):
+    def __data_ingest(self, payload, table):
         if not os.getenv('EVENT_ID'):
-            logging.error(f'Attempt made to send data without an event_id cached.')
-            return
-        
-        logging.info(f'Data received. Inserting to Database now...')
-        if not isinstance(payload, bytes):
-            try:
-                payload = pickle.loads(payload)
-                logging.info('Pickle Payload received, likely coming from debug source...')
-            except pickle.UnpicklingError:
-                payload = json.loads(payload.decode().replace("'", '"'))
-        db_desc = get_table_column_specs(force=True)
+            logging.error(f'\tAttempt made to send data without an event_id cached.')
+
+        logging.info(f'\tData received. Inserting to Database now...')
+        try:
+            data_dict = pickle.loads(payload)
+            logging.info('\tPickle Payload received, likely coming from debug source...')
+        except pickle.UnpicklingError:
+            data_dict = json.loads(payload.decode().replace("'", '"'))
+        data_dict['event_id'] = os.environ['EVENT_ID']
+        DBHandler.insert(table, target='PROD', user='electric', data=data_dict)
+
+    def __b64_ingest(self, payload, high_freq: bool):
+        if not os.getenv('EVENT_ID'):
+            logging.error(f'\tAttempt made to send data without an event_id cached.')
+
+        logging.info(f'\tData received. Inserting to Database now...')
         data_dict = self.base64_decode(payload, True)
         data_dict = self.preprocess_payload(data_dict, high_freq)
+        db_desc = get_table_column_specs(force=True)
         for table in ['dynamics', 'controls', 'pack', 'diagnostics', 'thermal']:
             DBHandler.insert(table, target='PROD', user='electric',
                              data={col: data_dict[col] for col in db_desc[table] if col in data_dict})
 
-    @staticmethod
-    def base64_decode(payload: str, high_freq: bool) -> dict:
+    def __base64_decode(payload: str, high_freq: bool) -> dict:
         bytes_data = bytearray(base64.b64decode(payload))
 
         with open(f'car_configs/version{bytes_data[0]:02}.json', 'r') as file:
@@ -122,8 +138,7 @@ class MQTTHandler:
             del payload['since_rtc']
         except KeyError:
             raise KeyError('RTC Start was not set.')
-        # payload['event_id'] = os.environ['EVENT_ID']
-        payload['event_id'] = 1
+        payload['event_id'] = os.environ['EVENT_ID']
         if high_freq:
             payload['gps'] = tuple(val / 60 for val in payload['gps'])
             payload['vcu_flags_json'] = {
@@ -160,7 +175,6 @@ class MQTTHandler:
             payload['brw_speed'] = payload['wheel_speed'][9:12]
             del payload['wheel_speed']
 
-
         return payload
 
     # TODO: Implement with enter for auto disconnect
@@ -180,13 +194,9 @@ def main():
     # mqtt.connect('ec2-52-14-184-219.us-east-2.compute.amazonaws.com')
     # mqtt.publish('config/flask', json.dumps({'event_id': 'idk'}, indent=4))
 
-    os.environ['RTC_START'] = str((datetime.datetime.now().timestamp() - 3600 * 3) * 1000)
     mqtt = MQTTHandler('ingest')
-    # data = mqtt.base64_decode(b'AUQWAAAAAL4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABdAWYBAgBfAToBQgFxAEkxFgGMSYH8EgDZagAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=', True)
-    data = mqtt.base64_decode(b'AQAAAAAAqqNAAAAAADCNCkEAAAAAANbZQAAAAABQbBFBAAAAAMAe50AAAAAAgFnSQAAAAADc5UVBAAAAANqkQkEAAAAA3vRIQQAAAIAk8X5BAAAAAIhfXUEAAAAAwZRtQQAAAAC2AUFBAAAAABTeMkEAAAAAWJ8sQQAAAAAE9jRBAAAAAMjgNUEAAAAAAIBJQAAAAKCGTa1BAAAAAADAWUAAAACA0ft7QQAAAADz62tBAAAAACu/akEAAAAAWR1hQQAAAAAcBmpBAAAAAFBFdUEAAAAARgtYQQAAAIBQ/KRBAAAAQFwhn0EAAAAASAVCQQAAAACmfUNBAAAAAG5YYUEAAACA34JzQQAAAADsAVxBAAAAAKj1cUEAAAAAvTBiQQAAAAB3iWNBAAAAAPnwakEAAAAAT6dtQQAAAAD2O1lBAAAAAIABX0EAAAAARB1cQQAAAACIKlFBAAAAADtja0EAAACApO50QQAAAIDe0n5BAAAAgMB4eUEAAAAAy85qQQAAAADskGlBAAAAADSLaEEAAACAPsl9QQAAAABNE3NBAAAAAL4VQ0EAAAAAYilFQQAAAADYJTFBAAAAADgwRUEAAAAA9qpDQQAAAADoKCdBAAAAAEglNUEAAAAAsOsYQQAAAADAavtAAAAAAEAvDUEAAAAAYKcLQQAAAACIZRJBAAAAAAAMp0AAAAAAEPojQQAAAACgWTVBAAAAAEBA3UAAAAAAwKT9QA==', True)
-    print(mqtt.preprocess_payload(data))
-    # mqtt.connect('mosquitto')
-    # mqtt.subscribe(topic='#')
+    mqtt.connect('mosquitto')
+    mqtt.subscribe(topic='#')
 
 
 if __name__ == '__main__':
