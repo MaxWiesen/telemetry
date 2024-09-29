07566be401ebc631c95c5358ee0ec843c1c943d7
Max Wiesenfeld <maw5859@utexas.edu>
2024-09-29 10:44:42 -0500

    Minor changes

--- stack/processors/processor.py
+++ stack/processors/processor.py
@@ -1,6 +1,10 @@
 import os
 import logging
 from time import sleep
+import pandas as pd
+import numpy as np
+
+from stack.ingest.mqtt_handler import MQTTHandler
 
 if os.getenv('IN_DOCKER'):
     from db_handler import DBHandler, get_table_column_specs    # Cheesed import statement using bind mount
@@ -9,17 +13,24 @@ else:
 
 
 class DBProcessor:
-    def __init__(self, db_handler: DBHandler):
-        self.handler = db_handler
+    def __init__(self, db_handler: DBHandler=None):
+        self.handler = db_handler if db_handler else DBHandler()
 
     def check_alive(self):
         logging.info(self.handler.simple_select('''SELECT event_id FROM event WHERE status = 1''', handler=self.handler))
 
+    def sliding_window(self, f, query, window_size):
+        self.handler.simple_select(query, handler=self.handler)
+
+
+def main():
+    with MQTTHandler() as mqtt:
+        processor = DBProcessor()
+        while True:
+            processor.check_alive()
+            sleep(5)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    handler = DBHandler()
-    processor = DBProcessor(handler)
-    while True:
-        processor.check_alive()
-        sleep(5)
+    main()
