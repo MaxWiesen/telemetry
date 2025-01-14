4e513d3aacb90226f48d07a5651de996cc82808e
Terence Dumas <dumast@awty.org>
2025-01-14 14:46:10 -0600

    debugging on gps classifier

--- stack/processors/gps_classifier/gps_classifier.py
+++ stack/processors/gps_classifier/gps_classifier.py
@@ -12,6 +12,10 @@ from psycopg import logger
 import requests
 from enum import Enum
 from numpy.typing import NDArray
+import warnings
+
+warnings.simplefilter('ignore', np.linalg.LinAlgError)
+
 
 
 if os.getenv('IN_DOCKER'):
@@ -65,6 +69,7 @@ class GPSClassifierProcessor:
                 "end_time": time,
                 "notes": f"{process_to_end.type.name}"
         }
+        print("DB OBJ: ", db_obj)
         DBHandler.insert(table="classifier", data=db_obj, target=DBTarget.LOCAL, user="electric", handler=self.handler)
         self.current_process = None
         
@@ -87,32 +92,63 @@ class GPSClassifierProcessor:
         packet = points[:, 4]
         
         # Not enough time between event starts
-        if len(self.processes != 0) and time[0] < self.processes[-1].starting_time + check_delay * 1000:
+        if len(self.processes) > 0 and time[0] < self.processes[-1].starting_time + check_delay * 1000:
             return
         
         # Look for high spike in torque request
         if not self.current_process or self.current_process.type != ProcessType.LINEAR_ACCELERATION:
-            smoothed_torque_request = np.polyfit(time, torque_request, 1)
-             
-            if smoothed_torque_request[0] > la_threshold:
+            # smoothed_torque_request = np.polyfit(time, torque_request, 1)
+            
+            # if smoothed_torque_request[0] > la_threshold:
+            #     starting_packet: int = packet[-1]
+            #     starting_time: int = time[-1]
+            #     print("STARTING TIME: ", starting_time)
+            #     logging.info(f"HIGH TORQUE REQUEST ACHIEVED AT {starting_time}: {smoothed_torque_request[0]}")
+            #     target_time = starting_time + la_time_window * 1000
+            #     new_process = Process(type=ProcessType.LINEAR_ACCELERATION, starting_packet=starting_packet, starting_time=starting_time, target_time=target_time)
+            #     self.processes.append(new_process)
+            #     return
+            # logging.info(f"HIGH TORQUE REQUEST NOT ACHIEVED: {smoothed_torque_request[0]}. TRASHING RESULTS")
+            
+            avg_torque_request = np.average(torque_request)
+            
+            if avg_torque_request > la_threshold:
                 starting_packet: int = packet[-1]
                 starting_time: int = time[-1]
+                logging.info(f"STARTING TIME: {starting_time}")
+                logging.info(f"HIGH TORQUE REQUEST ACHIEVED AT {starting_time}: {avg_torque_request}")
                 target_time = starting_time + la_time_window * 1000
                 new_process = Process(type=ProcessType.LINEAR_ACCELERATION, starting_packet=starting_packet, starting_time=starting_time, target_time=target_time)
                 self.processes.append(new_process)
                 return
+            logging.info(f"HIGH TORQUE REQUEST NOT ACHIEVED: {avg_torque_request}. TRASHING RESULTS")
+
                 
         # Look for high spike in steer voltage
         if not self.current_process or self.current_process.type != ProcessType.TURN:
-            smoothed_steer_voltage = np.polyfit(time, steer_v, 1)
+            # smoothed_steer_voltage = np.polyfit(time, steer_v, 1)
             
-            if smoothed_steer_voltage[0] > t_threshold:
+            # if smoothed_steer_voltage[0] > t_threshold:
+            #     starting_packet: int = packet[-1]
+            #     starting_time: int = time[-1]
+            #     logging.info(f"HIGH STEER REQUEST ACHIEVED AT {starting_time}: {smoothed_steer_voltage[0]}")
+            #     target_time = starting_time + t_time_window * 1000
+            #     new_process = Process(type=ProcessType.TURN, starting_packet=starting_packet, starting_time=starting_time, target_time=target_time)
+            #     self.processes.append(new_process)
+            #     return
+            # logging.info(f"HIGH STEER REQUEST NOT ACHIEVED: {smoothed_steer_voltage[0]}. TRASHING RESULTS")
+            
+            avg_steer_voltage = np.average(steer_v) - 1.25 # 1.25 = center
+            
+            if abs(avg_steer_voltage) > t_threshold:
                 starting_packet: int = packet[-1]
                 starting_time: int = time[-1]
+                logging.info(f"HIGH STEER REQUEST ACHIEVED AT {starting_time}: {avg_steer_voltage}")
                 target_time = starting_time + t_time_window * 1000
                 new_process = Process(type=ProcessType.TURN, starting_packet=starting_packet, starting_time=starting_time, target_time=target_time)
                 self.processes.append(new_process)
                 return
+            logging.info(f"HIGH STEER REQUEST NOT ACHIEVED: {avg_steer_voltage}. TRASHING RESULTS")
         
     def process_thread(self, frequency: int, la_threshold: float, t_na_threshold: float, t_h_threshold: float):
         """
@@ -131,8 +167,9 @@ class GPSClassifierProcessor:
             self.current_process = self.processes[0]
             self.processes.pop(0)
             points: NDArray = []
+            logging.info(f"LOADED PROCESS {self.current_process.type} STARTING AT {self.current_process.starting_time}")
             while True:
-                points = np.array(DBHandler.simple_select("""
+                points = np.array(DBHandler.simple_select(f"""
                     WITH next_time AS (
                         SELECT MIN(time) as min_time
                         FROM packet
@@ -141,72 +178,84 @@ class GPSClassifierProcessor:
                     SELECT d.gps_heading, d.body3_accel, p.time
                     FROM dynamics d
                     JOIN packet p ON p.packet_id = d.packet_id
-                    WHERE (SELECT min_time FROM next_time) IS NOT NUL
+                    WHERE (SELECT min_time FROM next_time) IS NOT NULL
                     AND p.time <= (SELECT min_time FROM next_time)
                     And p.time >= {self.current_process.starting_time}
                     ORDER BY p.time
+                    ASC
                     LIMIT 500
-                    """, handler=self.handler, target=DBTarget.LOCAL))
-                if points != None: break
+                    """, handler=self.handler, target=DBTarget.LOCAL), dtype=object)
+                if points.any(): break
                 sleep(1 / frequency)
+                
+            logging.info(f"ALL POINTS FOUND. STARTING PROCESSING.")
             
             body3_accel = points[:, 1]
+            body3_accel = np.array([list(row) for row in body3_accel])
             tangential_accel = body3_accel[:, 0]
             normal_accel = body3_accel[:, 1]
-            heading = points[:, 0]
-            time = points[:, 2]
+            heading = np.asarray(points[:, 0], dtype=np.float64)
+            time = np.asarray(points[:, 2], dtype=np.float64)
             
             if self.current_process.type == ProcessType.LINEAR_ACCELERATION:
                 smoothed_normal_accel = np.polyfit(time, normal_accel, 1)
                 smoothed_heading = np.polyfit(time, heading, 1)
                 if smoothed_normal_accel[0] > t_na_threshold or smoothed_heading[0] > t_h_threshold:
                     self._stop_process(self.current_process.starting_time)
+                    logging.info(f"THRESHOLD EXCEEDED")
+                else:
+                    logging.info(f"THRESHOLD NOT EXCEEDED. TRASHING INSTANCE.")
+                    
 
             
             if self.current_process.type == ProcessType.TURN:
                 smoothed_tangential_accel = np.polyfit(time, tangential_accel, 1)
                 if smoothed_tangential_accel[0] > la_threshold:
                     self._stop_process(self.current_process.starting_time)
+                    logging.info(f"THRESHOLD EXCEEDED")
+                else:
+                    logging.info(f"THRESHOLD EXCEEDED")
+                    logging.info(f"THRESHOLD NOT EXCEEDED. TRASHING INSTANCE.")
                     
-                
-            
-            
-            
+                    
    
-    def run_thread(self, frequency: int, window_size: int, la_threshold: float, t_threshold: float, la_time_window: float, t_time_window: float):
+    def run_thread(self, frequency: int, window_size: int, la_threshold: float, t_threshold: float, la_time_window: float, t_time_window: float, debug=True):
         """Sliding Window"""
         
         while True:
             # Event not properly set
-            if not self.event_id or self.status != 1:
+            if not debug and (not self.event_id or self.status != 1):
                 logging.info("No Event Started")
                 sleep(1 / frequency)
                 continue
             
             # Not enough points
-            if len(points) < window_size:
+            if not debug and len(points) < window_size:
                 logging.warning("Not enough points for computation. Trashing the instance")
                 sleep(1 / frequency)
                 continue
             
             # Suspicious time deltas
             MAX_TIME_DELTA = 5 * 1000
-            if points[len(points) - 1][1] - points[0][1] < MAX_TIME_DELTA:
+            if not debug and points[len(points) - 1][1] - points[0][1] < MAX_TIME_DELTA:
                 logging.error(f"Interval is suspicious: {points[len(points) - 1][1] - points[0][1]}ms. Trashing the instance")
                 sleep(1 / frequency)
                 continue
             
-            points  = np.array(DBHandler.simple_select(f"SELECT d.gps_velocity, d.torque_request, c.steer_v, p.time FROM dynamics d JOIN packet p ON p.packet_id = d.packet_id JOIN controls c ON c.packet_id = d.packet_id WHERE d.packet_id >= {self.start_packet} ORDER BY d.packet_id DESC LIMIT {window_size}", handler=self.handler, target=DBTarget.LOCAL))
-            # [0]: gps_velocity, [1]: gps_heading, [2]: body3_accel(pdu), [3]: torque_request, 
+            # logging.info(self.start_packet)
+            points  = np.array(DBHandler.simple_select(f"SELECT d.gps_velocity, d.torque_request, c.steer_v, p.time, p.packet_id FROM dynamics d JOIN packet p ON p.packet_id = d.packet_id JOIN controls c ON c.packet_id = d.packet_id WHERE d.packet_id >= {self.start_packet} ORDER BY d.packet_id ASC LIMIT {window_size}", handler=self.handler, target=DBTarget.LOCAL))
+            # logging.info(points[0][3])
             
-            # Checks if at rest
-            if math.abs(points[:, 0].mean()) < self.VELOCITY_THRESHOLD:
-                sleep(1 / frequency)
-                self._stop_process(points[:, 3][-1])
-                continue
-            
-            self._detect_events(points=points, la_threshold=la_threshold, t_threshold=t_threshold, la_time_window=la_time_window, t_time_window=t_time_window, check_delay=2)
-        
+            if len(points) > 0:
+                # Checks if at rest
+                if abs(points[:, 0].mean()) < self.VELOCITY_THRESHOLD:
+                    sleep(1 / frequency)
+                    self._stop_process(points[:, 3][-1])
+                else:
+                    self._detect_events(points=points, la_threshold=la_threshold, t_threshold=t_threshold, la_time_window=la_time_window, t_time_window=t_time_window, check_delay=2)
+                # increment window by half size
+                self.start_packet = points[int(len(points) / 2)][4]
+
             sleep(1 / frequency)
         
     def on_message(self, client: mqtt_client.Client, userdata, msg):
@@ -233,17 +282,17 @@ def run_processor():
         processor = GPSClassifierProcessor(db_handler=handler)
         
         
-        frequency = 1
+        frequency = 500
         window_size = 50
-        la_threshold=1 
-        t_threshold=1
+        la_threshold = 8
+        t_threshold=0.75
         la_time_window=10
         t_time_window=10
         t1 = threading.Thread(target=processor.run_thread, args=(frequency, window_size, la_threshold, t_threshold, la_time_window, t_time_window))
         t1.start()
         
         
-        frequency = 1
+        frequency = 100
         la_threshold = 1
         t_na_threshold = 1
         t_h_threshold = 1
